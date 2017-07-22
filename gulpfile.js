852b0db332839586cd1efde43e369f02a0ea0ef0
Luciano Baraúna <baraunaluciano@Mac-mini-de-Luciano.local>
2017-07-22 15:29:27 -0300

    add files code

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require( 'gulp' );
+const gulpUtil = require( 'gulp-util' );
+const pug = require( 'gulp-pug' );
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require( 'browser-sync' ).create();
+
+gulp.task( 'taskPug', function buildHtml(){
+    return gulp.src('src/*.pug')
+               .pipe( pug({
+                   pretty: true
+               }) )
+               .pipe( gulp.dest('dist/') )
+               .on( 'error', gulpUtil.log )
+               .pipe( browserSync.stream() )
+});
+
+gulp.task( 'taskSass', function buildSass(){
+    return gulp.src('sass/**/*.scss')
+               .pipe(sass({
+                            style: 'expanded',
+                            sourceComments: 'map',
+                            errLogToConsole: true
+                         }))
+               .pipe(autoprefixer( 'last 2 version', '> 1%', 'ie 10' ))
+               .pipe( gulp.dest( 'dist/css' ))
+               .on('error', gulpUtil.log)
+               .pipe( browserSync.stream() );
+               
+});
+
+gulp.task( 'initServer', ['taskPug'], function startServe(){
+    
+    browserSync.init({
+       server: './dist' 
+    });
+
+    gulp.watch( 'src/*.pug', ['taskPug']);
+    gulp.watch( 'src/**/*.scss', ['taskSass']);
+    gulp.watch( 'dist/*.html' )
+        .on( 'change', browserSync.reload );
+
+});
